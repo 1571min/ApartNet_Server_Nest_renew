993660008fd20ac0dc9cdebd4e6bf28f08e89efc
1571min <1571min@naver.com>
2021-01-17 20:48:10 +0900

    add dotenv setting and typeorm setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,42 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { AuthModule } from './auth/auth.module'
-import { UserModule } from './user/user.module'
-import { MessageModule } from './message/message.module'
-import { BoardModule } from './board/board.module'
-import { CommentModule } from './comment/comment.module'
+import { AuthModule } from './modules/auth/auth.module'
+import { UserModule } from './modules/user/user.module'
+import { MessageModule } from './modules/message/message.module'
+import { BoardModule } from './modules/board/board.module'
+import { CommentModule } from './modules/comment/comment.module'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import configOption from './config/configOption'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { join } from 'path'
 
 @Module({
-  imports: [AuthModule, UserModule, MessageModule, BoardModule, CommentModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configOption]
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB.HOST'),
+        port: configService.get('DB.PORT'),
+        username: configService.get('DB.USERNAME'),
+        password: configService.get('DB.PASSWORD'),
+        database: configService.get('DB.DATABASE'),
+        entities: [join(__dirname, '/modules/**/*.entity.js')],
+        synchronize: false
+      })
+    }),
+    AuthModule,
+    UserModule,
+    MessageModule,
+    BoardModule,
+    CommentModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
---
+++ src/config/configOption.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config'
+
+export default () => ({
+  APP: {
+    PORT: Number(process.env.SERVICE_PORT) || 3000,
+    cors: (process.env.CORS_ORIGINS || '').split(',')
+  },
+  DB: {
+    HOST: process.env.DATABASE_HOST,
+    PORT: Number(process.env.DATABASE_PORT) || 33060,
+    USERNAME: process.env.DATABASE_USERNAME,
+    PASSWORD: process.env.DATABASE_PASSWORD,
+    DATABASE_NAME: process.env.DATABASE_NAME
+  }
+})
